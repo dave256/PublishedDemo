87de6cb0bea2b57cbe30634f7397e3e4fd227097
Dave Reed <dave256@mac.com>
2021-01-20 19:11:50 -0500

    Initial Commit

---
+++ PublishedDemo/PublishedDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublishedDemoApp.swift
+//  PublishedDemo
+//
+//  Created by David M Reed on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct PublishedDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
